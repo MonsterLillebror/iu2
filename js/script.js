2c3d8e536e2bcbb3db2217337e0540731f7f1129
MonsterLillebror <d.alfaro.molina@gmail.com>
2022-11-18 14:14:10 +0100

    script error

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+const checkLength = (element,minLenght = 2) => {
+    console.log(element.target.id)
+
+    if (element.target.value.length < minLenght) {
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = "You must enter a name"
+    }
+
+}
+
+
+
 const validate = (e) => {
-    console.log(e)
+    switch(e.target.type) {
+        case "text":
+            break;
+        case "email":
+            break;
+        case "textarea":
+            break;
+    }
 }
