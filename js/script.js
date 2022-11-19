fb7201f5e51b9db23ee31eda227442001d4ab874
MonsterLillebror <d.alfaro.molina@gmail.com>
2022-11-19 23:55:25 +0100

    form från början

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-const checkLength = (element,minLenght = 2) => {
-    console.log(element.target.id)
-
-    if (element.target.value.length < minLenght) {
-        document.getElementById(`${element.target.id}ErrorMessage`).innerText = "You must enter a name"
-    }
-
-}
-
-
-
 const validate = (e) => {
-    switch(e.target.type) {
-        case "text":
-            break;
-        case "email":
-            break;
-        case "textarea":
-            break;
-    }
+    console.log(e.target.type)
 }
