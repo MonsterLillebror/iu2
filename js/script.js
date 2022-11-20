05c7c9fa214ff8d06bc25efd5384d5d172c18a3f
MonsterLillebror <d.alfaro.molina@gmail.com>
2022-11-20 02:35:12 +0100

    forum check test

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const checkLength = (element, errorMessage = "This field is required", minLength = 2) => {
+const checkLength = (element, errorMessage = "This field is required", minLength = 1) => {
     console.log(element.target.id)
     if (element.target.value.length < minLength ) {
         document.getElementById(element.target.id).classList.add('error')
@@ -10,16 +10,27 @@ const checkLength = (element, errorMessage = "This field is required", minLength
     }
 }
 
+const checkEmail = (element, errorMessage = "Your must enter an valid e-mail address") => {
+    if (!element.target.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = errorMessage      
+    }
+    else {
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""  
+    }
+}
+
 const validate = (e) => {
     switch(e.target.type) {
         
         case "text":
-            checkLength(e)
+            checkLength(e, `Your must enter a ${e.target.id}`, 2)
             break;
         case "email":
             break;
         case "textarea":
-            checkLength(e, 10)
+            checkLength(e, `Your must enter a ${e.target.id}`, 5)
             break;
 
     }
