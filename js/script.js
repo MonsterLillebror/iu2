9c0ce27cdf47b159c19825fec88b0d873e197bec
MonsterLillebror <d.alfaro.molina@gmail.com>
2022-11-20 01:18:52 +0100

    forum halva

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+const checkLength = (element, minLength = 2) => {
+    console.log(element.target.id)
+    if (element.target.value.length < minLength ) {
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = "You must enter a name"  
+    }
+    else {
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""  
+    }
+}
+
 const validate = (e) => {
-    console.log(e.target.type)
+    switch(e.target.type) {
+        
+        case "text":
+            checkLength(e)
+            break;
+        case "email":
+            break;
+        case "textarea":
+            break;
+
+    }
 }
